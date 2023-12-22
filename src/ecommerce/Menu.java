5fc33ce4d2b2345d49f6bd0bfbc56f6a1783bf7e
Sara Albuquerque <saraalbuquerque@live.com>
2023-12-22 09:15:14 -0300

    primeiro commit

---
+++ src/ecommerce/Menu.java
@@ -0,0 +1,34 @@
+package ecommerce;
+
+import ecommerce.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+
+		while(true) {
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "*****************************************************" + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + "                                                     " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_WHITE +"             ◣  BANCO DO BRAZIL COM Z  ◥             " +Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + "                                                     " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "*****************************************************" + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "                                                     " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "             1 - Criar Conta                         " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             2 - Listar todas as Contas              " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             3 - Buscar Conta por Numero             " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             4 - Atualizar Dados da Conta            " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             5 - Apagar Conta                        " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             6 - Sacar                               " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             7 - Depositar                           " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             8 - Transferir valores entre Contas     " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "             9 - Sair                                " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW +  "                                                     " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_YELLOW + "*****************************************************" + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_WHITE + "Entre com a opção desejada:                          " + Cores.RESET);
+        System.out.println(Cores.ANSI_BLACK_BACKGROUND +Cores.TEXT_YELLOW + "                                                     " + Cores.RESET);
+        
+        
+
+	}
+
+}
